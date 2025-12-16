7589f86554ae29354c627b84917cc28e44c253f4
Asmi Walia <waliaasmii@gmail.com>
2025-12-16 13:31:49 +0530

    bugs

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/assets";
 const Navbar = ({ setToken }) => {
   return (
     <header className='flex py-2 px-[4%] justify-between'>
-      <img className='w-[max(10%,80px)]' src={assets.logo} alt='logo' />
+      <h1 className="text-4xl font-bold">AURA</h1>
       <button onClick={() => setToken("")}
         className='bg-gray-600 text-white px-3 py-2 sm:px-7 sm:py-1 rounded-full'
       >
