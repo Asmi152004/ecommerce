8508f4dcd88730d005104230e1967270d6688e80
Asmi Walia <waliaasmii@gmail.com>
2026-01-14 00:38:41 +0530

    bug

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "https://ecommerce-5dzc.onrender.com",
-  "https://ecommerce-h792.vercel.app"
+  "https://ecommerce-h792.vercel.app",
+  "http://localhost:5174"
 ];
 
 const corsOptions = {
